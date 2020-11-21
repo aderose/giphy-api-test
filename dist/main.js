77ffed6041abae2d3ea0ab4a188df9bfbf9ba119
aderose <sandro.derose28@yahoo.com>
2020-11-21 13:53:38 +0000

    Implement gif search form and next button

--- dist/main.js
+++ dist/main.js
@@ -7,11 +7,94 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/config.js":
+/*!***********************!*\
+  !*** ./src/config.js ***!
+  \***********************/
+/*! namespace exports */
+/*! export default [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_exports__, __webpack_require__.r, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\n  API_KEY: 'fj5ziE4ILPricdqb0WywRB5AbMGrVXLn'\n});\n\n//# sourceURL=webpack://api-test/./src/config.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! unknown exports (runtime-defined) */
-/*! runtime requirements:  */
-eval("var img = document.querySelector('img');\nfetch('https://api.giphy.com/v1/gifs/translate?api_key=fj5ziE4ILPricdqb0WywRB5AbMGrVXLn&s=cats').then(function (response) {\n  return response.json();\n}).then(function (response) {\n  img.src = response.data.images.original.url;\n}).catch(function (e) {\n  console.log(e);\n});\n\n//# sourceURL=webpack://api-test/./src/index.js?");
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _config__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./config */ \"./src/config.js\");\n\n\n(function gifGenerator(doc) {\n  var counter = 0;\n  var gifs = []; // get gifs from giphy using the provided keyword\n\n  function fetchGifs(keyword) {\n    return fetch(\"https://api.giphy.com/v1/gifs/search?api_key=\".concat(_config__WEBPACK_IMPORTED_MODULE_0__.default.API_KEY, \"&q=\").concat(keyword)).then(function (response) {\n      return response.json();\n    }).then(function (response) {\n      return response.data;\n    }).catch(function (e) {\n      console.log(e);\n    });\n  } // load a new array of gifs\n\n\n  function getNewGifs(keyword) {\n    fetchGifs(keyword).then(function (data) {\n      gifs = data;\n      counter = 0;\n      doc.querySelector('img').src = gifs[counter].images.original.url;\n    });\n  } // listen for a form submit\n\n\n  doc.querySelector('form').addEventListener('submit', function (e) {\n    e.preventDefault();\n    var keyword = doc.querySelector('#search').value;\n    if (keyword === '') keyword = 'random';\n    getNewGifs(keyword);\n  }); // update image when next button is clicked\n\n  doc.querySelector('#next').addEventListener('click', function () {\n    counter = (counter + 1) % gifs.length;\n    doc.querySelector('img').src = gifs[counter].images.original.url;\n  });\n  getNewGifs('magic');\n})(document);\n\n//# sourceURL=webpack://api-test/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-const img = document.querySelector('img');
+import config from './config';
 
-fetch(
-  'https://api.giphy.com/v1/gifs/translate?api_key=fj5ziE4ILPricdqb0WywRB5AbMGrVXLn&s=cats'
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((response) => {
-    img.src = response.data.images.original.url;
-  })
-  .catch((e) => {
-    console.log(e);
+(function gifGenerator(doc) {
+  let counter = 0;
+  let gifs = [];
+
+  // get gifs from giphy using the provided keyword
+  function fetchGifs(keyword) {
+    return fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${config.API_KEY}&q=${keyword}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  // load a new array of gifs
+  function getNewGifs(keyword) {
+    fetchGifs(keyword).then((data) => {
+      gifs = data;
+      counter = 0;
+      doc.querySelector('img').src = gifs[counter].images.original.url;
+    });
+  }
+
+  // listen for a form submit
+  doc.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let keyword = doc.querySelector('#search').value;
+    if (keyword === '') keyword = 'random';
+    getNewGifs(keyword);
   });
+
+  // update image when next button is clicked
+  doc.querySelector('#next').addEventListener('click', () => {
+    counter = (counter + 1) % gifs.length;
+    doc.querySelector('img').src = gifs[counter].images.original.url;
+  });
+
+  getNewGifs('magic');
+})(document);
